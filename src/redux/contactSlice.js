3a17bacc9a9cc9991d956761f4b4faf7147b2144
Andriy323 <108237550+Andriy323@users.noreply.github.com>
2023-03-05 15:27:59 +0200

    delete import axios

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,6 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-import axios from 'axios';
 import { getContactsApi, deleteContactApi, addContactApi } from 'helpers/fetch';
 
 export const fetchContacts = createAsyncThunk(
