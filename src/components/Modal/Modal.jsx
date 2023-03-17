a4f2874bd926424e9048732768ab3d3f30b3ac49
Andriy323 <108237550+Andriy323@users.noreply.github.com>
2023-03-17 15:34:30 +0200

    add btn close

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -41,6 +41,7 @@ const Modal = ({ close, contact }) => {
   return createPortal(
     <div className={style.overlay} onClick={modalClose}>
       <div className={style.modal}>
+        <button className={style.btnClose} onClick={modalClose}>X</button>
         <form onSubmit={hendleSubmit} className={style.form}>
           <label className={style.label} htmlFor={idName}>
             Name
